0d6c71d4cd9e91a0e96ef3059b6c683deff2a2dd
Johnny Navarro <j.navamo87@gmail.com>
2025-03-19 19:47:15 -0600

    chore(variables): add football-game excersice

---
+++ Fundamentals/variables/football-game.js
@@ -0,0 +1,38 @@
+/*
+El evento es parte de las Eliminatorias Sudamericanas para la Copa del Mundo.
+En un partido celebrado el 22/11/23 a las 20:30, el equipo local Argentina se enfrento al 
+equipo visitante Brasil. El resultado final fue 1-0 indicando a Argentina como quipo
+ganador. 
+*/
+
+const eventName = "Eliminatorias Sudamericanas"
+
+const day = 22
+const month = 11
+const year = 2023
+
+const date = `${day}/${month}/${year}`
+
+const hour = 20
+const minutes = 30
+
+const time = `${hour}:${minutes}`
+
+const formatHour = hour - 12
+const formatTime = `${formatHour}:${minutes} PM` 
+
+const localTeam = "Argentina"
+const visitorTeam = "Brasil"
+
+const localTeamScore = 1
+const visitorTeamScore = 0
+
+const score = `${localTeamScore}-${visitorTeamScore}`
+
+
+console.log (`
+    Torneo: ${eventName} ${time}\n
+    Equipos: ${localTeam} vs ${visitorTeam}\n
+    Resultado: ${score}\n   
+`)
+
