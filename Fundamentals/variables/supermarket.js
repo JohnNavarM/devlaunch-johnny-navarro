9e611b2e1042316275817dacad9fa0c81a47f4b1
Johnny Navarro <j.navamo87@gmail.com>
2025-03-21 17:00:58 -0600

    chore(variables): add gym-membership excersice

---
+++ Fundamentals/variables/supermarket.js
@@ -0,0 +1,6 @@
+/*
+El ELite Center Fitness es un gimnasio boutique ubicado en Palo Alto, California, 
+que ofrece membresías por $150 al mes. 
+Opera de 6:00 a. m. a 10:00 p. m. Emplea a 10 entrenadores certificados 
+y cuenta con comidades como sauna, piscina y clases grupales.
+*/
