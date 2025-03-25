901b6c009adb1896e1b531729d12a5fb68dd6c48
Johnny Navarro <j.navamo87@gmail.com>
2025-03-25 08:27:47 -0600

    chore(variables): add supermarkte excersice)

--- Fundamentals/variables/supermarket.js
+++ Fundamentals/variables/supermarket.js
@@ -1,6 +1,34 @@
 /*
-El ELite Center Fitness es un gimnasio boutique ubicado en Palo Alto, California, 
-que ofrece membresías por $150 al mes. 
-Opera de 6:00 a. m. a 10:00 p. m. Emplea a 10 entrenadores certificados 
-y cuenta con comidades como sauna, piscina y clases grupales.
-*/
+Voy al supermercado GreenCenter para hacer mis compras habituales y 
+planeo comprar manzanas y naranjas. 
+Cuando estoy allí, veo que no hay plátanos ni uvas, asi que cambio un poco mi lista. 
+Veo que 3 cajeros están trabajando, así que me prepraro para pagar.
+Mis cosas cuestan 100 dólares en total. 
+Entrego $150 en efectivo y recibo $50 de cambio.
+*/  
+
+
+const supermarketName = "GreenCenter";
+
+const product1 = "manzanas";
+const product2 = "naranjas";
+
+const areBananasAvailable = false;
+const areGrapesAvailable = false;
+
+const numberOfActiveCashiers = 3;
+
+const cashInWalletDollars = 150;
+const totalCost = 100;
+const changeDollars = cashInWalletDollars - totalCost; 
+
+console.log(`
+    Supermarket: ${supermarketName} \n
+    Products: ${product1}, ${product2} \n
+    Bananas available: ${areBananasAvailable} \n
+    Grapes available: ${areGrapesAvailable} \n
+    Cashiers: ${numberOfActiveCashiers} \n
+    Total cost: $${totalCost} \n
+    Cash: $${cashInWalletDollars} \n
+    Change: $${changeDollars} \n 
+`)
