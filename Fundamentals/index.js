d74849b569a22fb0e48442f4a1a4e2b06c31d447
Johnny Navarro <j.navamo87@gmail.com>
2025-04-23 10:50:29 -0600

    feat(functions): add folder structure

--- Fundamentals/index.js
+++ Fundamentals/index.js
@@ -1,3 +1,16 @@
-if (0) {
-    console.log("Esto NO se ejecuta porque 0 es falsy");
+function printSomething (message = "Hello World") {
+  console.log(message);
 }
+
+printSomething ("hi"); 
+ 
+function saludar (nombre) {
+  return `Hola ${nombre}`;
+}
+console.log(saludar("Javier"));
+
+function area (base, altura) {
+  return base * altura;
+}
+
+console.log(area(10,6))
